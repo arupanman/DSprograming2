323b7a487dd805bae571af644e362485f36457c0
aruapnman <s2322089@stu.musashino-u.ac.jp>
2024-01-11 02:44:07 +0900

    スクレイピング.pyの編集(エンコーディングできるように)&tenki.csv1の作成

--- スクレイピング.py
+++ スクレイピング.py
@@ -16,4 +16,31 @@ data_list = soup.find_all('p', class_='indexes-telop-0')
 
 data= [a.get_text(strip=True) for a in data_list]
 
+# CSVファイルに保存
+converted_data = []
+for value in data:
+    if '暖房は必須！' in value:
+        converted_data.append(1)
+    elif '夜も朝も暖房' in value:
+        converted_data.append(2)
+    elif '寝る前に暖房' in value:
+        converted_data.append(3)
+    elif '翌朝少し寒い' in value:
+        converted_data.append(4)
+    elif 'よく眠れそう' in value:
+        converted_data.append(5)
+    else:
+        converted_data.append(0)  # 何も該当しない場合
+data=converted_data
 print(data)
+
+import os
+import pandas as pd
+data = pd.DataFrame({'睡眠指数': data})
+file_path = 'tenki1.csv'
+data.to_csv(file_path, index=False)
+
+
+save_dir = '/Users/aruha/DSprograming2/DSprograming2/'
+file_path = os.path.join(save_dir, 'tenki1.csv')
+data.to_csv(file_path, index=False)
